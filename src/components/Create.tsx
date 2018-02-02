19f996765e4fb0e8cd10427dfb773807e3ba38a5
Sam Jeston <sam.jeston@gmail.com>
2018-02-02 13:57:57 +1000

    Allowed importing of key

--- src/components/Create.tsx
+++ src/components/Create.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { AppState, View } from '../app'
 const StellarBase = require('stellar-sdk')
 
-export class CreateAccount extends React.Component<{setAccountKeys: Function, appState: AppState, changeView: Function}, undefined> {
+export class CreateAccount extends React.Component<{setAccountKeys: Function, appState: AppState, changeView: Function}, {privateKey: string, publicKey: string}> {
   constructor (props) {
     super(props)
   }
@@ -14,9 +14,43 @@ export class CreateAccount extends React.Component<{setAccountKeys: Function, ap
     this.props.changeView(View.dashboard, {walletId: 0})
   }
 
+  public importKeys() {
+    this.props.setAccountKeys(this.state.publicKey, this.state.privateKey)
+    // Call create wallet here...
+    this.props.changeView(View.dashboard, {walletId: 0})
+  }
+
+  public handleSumbit(e) {
+    e.preventDefault()
+    this.importKeys()
+  }
+
+  public handlePublic(ev) {
+    this.setState({publicKey: ev.target.value})
+  }
+
+  public handlePrivate(ev) {
+    this.setState({privateKey: ev.target.value})
+  }
+
   render() {
     return (
-      <button className='button' onClick={() => this.generate()}>Create Account</button>
+      <div className='columns'>
+        <div className='column'>
+          <h1>New Account</h1>
+          <button className='button' onClick={() => this.generate()}>Create Account</button>
+        </div>
+        <div className='column'>
+          <h1>Import Account</h1>
+          <form onSubmit={(ev) => this.handleSumbit(ev)}>
+            <label>Public Key</label>
+            <input onChange={(ev) => this.handlePublic(ev)} type='text' />
+            <label>Private Key</label>
+            <input onChange={(ev) => this.handlePrivate(ev)} type='text' />
+            <input className='button' type='submit' />
+          </form>
+        </div>
+      </div>
     )
   }
 }
