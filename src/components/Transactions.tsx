93cff19b3ee0c4c610d7b3e00064dedbf17e0605
Sam Jeston <sam.jeston@gmail.com>
2018-02-05 16:45:21 +1000

    Decoding transactions

--- src/components/Transactions.tsx
+++ src/components/Transactions.tsx
@@ -1,29 +1,91 @@
 import * as React from 'react'
 import { AppState } from '../app'
-import { getActiveWallet } from '../helpers/wallets';
+import { getActiveWallet } from '../helpers/wallets'
 const StellarSdk = require('stellar-sdk')
+import * as _ from 'lodash'
 
-export class Transactions extends React.Component<{appState: AppState}, {transactions: any[]}> {
+export interface HumanTransactions {
+  txId: string
+  txType: StellarTxType
+  txData: any
+  fee: number
+  date: Date
+  memo: string
+  signatures: string[]
+}
+
+export enum StellarTxType {
+  'Create Account' = 0,
+  'Payment' = 1,
+  'Path Payment' = 2,
+  'Manage Offer' = 3,
+  'Create Passive Offer' = 4,
+  'Set Options' = 5,
+  'Change Trust' = 6,
+  'Allow Trust' = 7,
+  'Account Merge' = 8,
+  'Inflation' = 9,
+  'Manage Data' = 10
+}
+
+export class Transactions extends React.Component<{appState: AppState}, {transactions: HumanTransactions[], currentPage: any, lastPage: boolean}> {
   constructor (props) {
     super(props)
-    this.state = { transactions: [] }
+    this.state = { transactions: [], lastPage: false, currentPage: undefined }
   }
 
   async componentDidMount() {
-    StellarSdk.Network.use(new StellarSdk.Network('Test SDF Network ; September 2015'))
+    await this.transactionPage()
+  }
 
+  // TODO: We need to update this method to display views for all transaction types
+  async transactionPage (): HumanTransactions[] {
+    StellarSdk.Network.use(new StellarSdk.Network('Test SDF Network ; September 2015'))
     const server = new StellarSdk.Server(this.props.appState.serverLocation, {allowHttp: true})
 
-    // TODO: Right a pager
-    const pageOne = await server.transactions().forAccount(getActiveWallet(this.props.appState).publicKey).call()
-    const pageTwo = await pageOne.next()
+    // Load 2 pages of records at a time, initializing if we do not yet have transactions
+    const currentPage = this.state.currentPage
+      ? this.state.currentPage
+      : await server.transactions().forAccount(getActiveWallet(this.props.appState).publicKey).call()
 
-    console.log(StellarSdk.xdr.TransactionResult.fromXDR(pageOne.records[2].result_xdr, 'base64'))
-    console.log(StellarSdk.xdr.TransactionMeta.fromXDR(pageOne.records[2].result_meta_xdr, 'base64'))
-    console.log(StellarSdk.xdr.TransactionEnvelope.fromXDR(pageOne.records[2].envelope_xdr, 'base64'))
+    const nextPage = await currentPage.next()
 
-    console.log(pageOne)
-    console.log(pageTwo)
+    if (nextPage.records.length === 0) {
+      this.setState({lastPage: true})
+    }
+
+    const records = currentPage.records.concat(nextPage.records)
+
+    return _.flatten(await Promise.all(records.map(async (r) => {
+      const operations = await r.operations()
+      return operations._embedded.records.map(o => {
+        return {
+          txId: r.id,
+          txType: StellarTxType[o.type_i],
+          txData: this.determineTxData(o),
+          date: new Date(r.created_at),
+          fee: r.fee_paid * 0.0000001
+        }
+      })
+    })))
+  }
+
+  determineTxData (operation: any) {
+    const ref = {
+      [StellarTxType['Create Account']]: {
+        'Funder': operation.funder,
+        'Starting Balance': operation.starting_balance,
+        'Account Created': operation.account
+      },
+      [StellarTxType['Payment']]: {
+        'Asset Type': operation.asset_type === 'native' ? 'Kinesis' : operation.asset_type,
+        'From': operation.from,
+        'To': operation.to,
+        'Amount': operation.amount
+      },
+    }
+
+    return ref[operation.type_i]
   }
 
   render() {
