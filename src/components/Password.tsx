cf03cc8a2a29d7302eda0d120c0913809da7de6e
Joel Olsson <joel.olsson@bullioncapital.com>
2018-02-07 10:35:30 +1000

    ABX-4062 add wallet lock functionality

--- src/components/Password.tsx
+++ src/components/Password.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { AppState } from '../app'
-import { getActiveWallet } from '../helpers/wallets'
+import { getActiveWallet, getPrivateKey } from '../helpers/wallets'
 
 export class Password extends React.Component<{appState: AppState, setPassword: Function}, {password: any}> {
   constructor (props) {
@@ -12,14 +12,46 @@ export class Password extends React.Component<{appState: AppState, setPassword: 
     this.props.setPassword(getActiveWallet(this.props.appState).publicKey, this.state.password)
   }
 
+  public unlockWallet(ev) {
+    ev.preventDefault()
+    this.setPassword()
+  }
+
+  public lockWallet(ev) {
+    ev.preventDefault();
+    this.setState({
+      password: ''
+    }, () => {
+      this.setPassword()
+    })
+  }
+
+  public privateKey() {
+    let activeWallet = getActiveWallet(this.props.appState) || {}
+    return getPrivateKey(this.props.appState, activeWallet)
+  }
+
   render() {
     return (
-      <form className='title-heading' onSubmit={(ev) => {ev.preventDefault(); this.setPassword()}} style={{ paddingBottom: '28px', paddingTop: '24px'}}>
-        <input className="input is-small" type="password" placeholder="Password" onChange={(e: any) => this.setState({password: e.target.value})} style={{display:'inline-block', maxWidth: '200px', padding: '17px 8px'}} />
-        <button type='submit' className='button' style={{display:'inline-block'}}>
+      <div>
+      {!this.privateKey() ?
+        (<form className='title-heading' onSubmit={this.unlockWallet.bind(this)} style={{ paddingBottom: '28px', paddingTop: '24px'}}>
+          <input className="input is-small" type="password" placeholder="Password" onChange={(e: any) => this.setState({password: e.target.value})} style={{display:'inline-block', maxWidth: '200px', padding: '17px 8px'}} />
+          <button type='submit' className='button' style={{display:'inline-block'}}>
             <i className='fas fa-unlock-alt' style={{marginRight:'6px'}}></i> Unlock Wallet
-        </button>
-      </form>
+          </button>
+        </form>)
+        :
+        (<form className='title-heading'
+          onSubmit={this.lockWallet.bind(this)}
+          style={{ paddingBottom: '28px', paddingTop: '24px'}}
+          >
+          <button type='submit' className='button' style={{display:'inline-block'}}>
+            <i className='fas fa-lock-alt' style={{marginRight:'6px'}}></i> Lock Wallet
+          </button>
+        </form>)
+      }
+      </div>
     )
   }
 }
