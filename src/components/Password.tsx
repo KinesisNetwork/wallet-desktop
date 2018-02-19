68857b65944dcee79822382feb68811176de3663
Joel Olsson <joel.olsson@bullioncapital.com>
2018-02-19 12:16:06 +1000

    Split password from view

--- src/components/Password.tsx
+++ src/components/Password.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react'
 import { AppState } from '../app'
-import { getActiveWallet, getActivePrivateKey } from '../helpers/wallets'
+import { getActiveWallet } from '../helpers/wallets'
 import * as swal from 'sweetalert'
 import { decryptPrivateKey } from '../services/encryption'
+import { PasswordPresentation } from './PasswordPresentation';
 
 export class Password extends React.Component<{appState: AppState, setPassword: Function}, {password: any}> {
   constructor (props) {
@@ -38,28 +39,20 @@ export class Password extends React.Component<{appState: AppState, setPassword: 
     })
   }
 
+  public setPasswordInput(password: string) {
+    this.setState({password: password})
+  }
+
   render() {
     return (
-      <div>
-      {!getActivePrivateKey(this.props.appState) ?
-        (<form className='title-heading' onSubmit={this.unlockWallet.bind(this)} style={{ paddingBottom: '28px', paddingTop: '24px'}}>
-          <input id='wallet-password' className="input is-small" value={this.state.password} type="password" placeholder="Password"
-            onChange={(e: any) => this.setState({password: e.target.value})} style={{display:'inline-block', maxWidth: '200px', padding: '17px 8px'}} />
-          <button type='submit' className='button' style={{display:'inline-block'}}>
-            <i className='fas fa-unlock-alt' style={{marginRight:'6px'}}></i> Unlock Wallet
-          </button>
-        </form>)
-        :
-        (<form className='title-heading'
-          onSubmit={this.lockWallet.bind(this)}
-          style={{ paddingBottom: '28px', paddingTop: '24px'}}
-          >
-          <button type='submit' className='button' style={{display:'inline-block'}}>
-            <i className='fas fa-lock-alt' style={{marginRight:'6px'}}></i> Lock Wallet
-          </button>
-        </form>)
-      }
-      </div>
+      <PasswordPresentation
+        appState={this.props.appState}
+        setPassword={this.props.setPassword}
+        unlockWallet={this.unlockWallet.bind(this)}
+        lockWallet={this.lockWallet.bind(this)}
+        setPasswordInput={this.setPasswordInput.bind(this)}
+        password={this.state.password}
+      />
     )
   }
 }
