0a81c3de3b1e1c77b0bad33502e54571a942415e
Carl Foster <carl.john.foster@gmail.com>
2018-03-01 10:59:31 +1000

    Fix up icon not changing

--- src/components/PasswordPresentation.tsx
+++ src/components/PasswordPresentation.tsx
@@ -11,50 +11,50 @@ export interface Props {
   lockWallet: Function
 }
 
+const LockedWallet: React.SFC<Props> = (props) => (
+  <form onSubmit={(ev) => props.unlockWallet(ev)}>
+    <div className='field has-addons has-addons-centered'>
+      <div className='control'>
+        <input id='wallet-password' className='input' value={props.password} type='password' placeholder='Password'
+          onChange={(e) => props.setPasswordInput(e.currentTarget.value)} />
+      </div>
+      <div className='control'>
+        <button type='submit' className='button'>
+          <span className='icon'>
+            <i className='fas fa-lock-open' />
+          </span>
+          <span>Unlock Wallet</span>
+        </button>
+      </div>
+    </div>
+  </form>
+)
+
+const UnlockedWallet: React.SFC<Props> = (props) => (
+  <form onSubmit={(ev) => props.lockWallet(ev)} >
+    <div className='field is-grouped is-grouped-centered'>
+      <div className='control'>
+        <button type='button' className='button' onClick={() => copyPrivateKey(props.appState)}>
+          <span className='icon'>
+            <i className='fas fa-copy' />
+          </span>
+          <span>Copy Private Key</span>
+        </button>
+      </div>
+      <div className='control'>
+        <button type='submit' className='button'>
+          <span className='icon'>
+            <i className='fas fa-lock' />
+          </span>
+          <span>Lock Wallet</span>
+        </button>
+      </div>
+    </div>
+  </form>
+)
+
 export const PasswordPresentation: React.SFC<Props> = (props) => (
   <div className='title-heading'>
-    {
-      !getActivePrivateKey(props.appState)
-      ? (
-      <form onSubmit={(ev) => props.unlockWallet(ev)}>
-        <div className='field has-addons has-addons-centered'>
-          <div className='control'>
-            <input id='wallet-password' className='input' value={props.password} type='password' placeholder='Password'
-              onChange={(e) => props.setPasswordInput(e.target.value)} />
-          </div>
-          <div className='control'>
-            <button type='submit' className='button'>
-              <span className='icon'>
-                <i className='fas fa-unlock-alt' />
-              </span>
-              <span>Unlock Wallet</span>
-            </button>
-          </div>
-        </div>
-      </form>
-        )
-      : (
-      <form onSubmit={(ev) => props.lockWallet(ev)} >
-        <div className='field is-grouped is-grouped-centered'>
-          <div className='control'>
-            <button type='button' className='button' onClick={() => copyPrivateKey(props.appState)}>
-              <span className='icon'>
-                <i className='fas fa-copy' />
-              </span>
-              <span>Copy Private Key</span>
-            </button>
-          </div>
-          <div className='control'>
-            <button type='submit' className='button'>
-              <span className='icon'>
-                <i className='fas fa-lock-alt' />
-              </span>
-              <span>Lock Wallet</span>
-            </button>
-          </div>
-        </div>
-      </form>
-        )
-    }
+    { !getActivePrivateKey(props.appState) ? <LockedWallet {...props} /> : <UnlockedWallet {...props} /> }
   </div>
 )
