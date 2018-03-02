c84e89fe0e11e223ac3abd697f5c17d782f7c2df
Carl Foster <carl.john.foster@gmail.com>
2018-03-02 11:46:00 +1000

    Fix up delete for unknown account

--- src/components/Delete.tsx
+++ src/components/Delete.tsx
@@ -23,14 +23,16 @@ export class Delete extends React.Component<{appState: AppState, setWalletList: 
     if (willDelete) {
       const activeWallet = getActiveWallet(this.props.appState)
       const server = new StellarSdk.Server(this.props.appState.connection.horizonServer, {allowHttp: true})
-      const account = await server.loadAccount(activeWallet.publicKey)
-      const kinesisBalance = account.balances.filter(b => b.asset_type === 'native')[0].balance
-      if (kinesisBalance > 0) {
-        const stillContinue = await getPasswordConfirmation(activeWallet)
-        if (!stillContinue) {
-          return
+      try {
+        const account = await server.loadAccount(activeWallet.publicKey)
+        const balance = Number(account.balances.find(b => b.asset_type === 'native').balance)
+        if (balance > 0) {
+          const stillContinue = await getPasswordConfirmation(activeWallet)
+          if (!stillContinue) {
+            return
+          }
         }
-      }
+      } catch (e) {null}
       deleteWallet(accountId)
         .then((wallets) => {
           this.props.setWalletList(wallets)
