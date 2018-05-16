57c4eeeaa32d860eb622518103bab72ae1dff9bd
Carl Foster <carl.john.foster@gmail.com>
2018-05-16 17:56:07 +1000

    Fix up path to exe

--- appveyor.ps1
+++ appveyor.ps1
@@ -1,8 +1,9 @@
+$outFile = "C:\projects\wallet-desktop\out\make\squirrel.windows\x64\kinesis-wallet-setup.exe";
 if ("${env:APPVEYOR_PULL_REQUEST_NUMBER}" -eq "") {
   npm run deploy -- --dry-run --tag="0.1.${env:APPVEYOR_BUILD_NUMBER}";
-  cmd.exe /c "`"${env:SIGNTOOL}`" sign /f ./code-sign.pfx /p %CERTIFICATE_PASSWORD% /fd sha1 /tr http://sha1timestamp.ws.symantec.com/sha1/timestamp /td sha1 /v .\out\make\squirrel.windows\x64\kinesis-wallet-setup.exe";
-  cmd.exe /c "`"${env:SIGNTOOL}`" sign /f ./code-sign.pfx /p %CERTIFICATE_PASSWORD% /fd sha256 /tr http://sha256timestamp.ws.symantec.com/sha256/timestamp /td sha256 /as /v .\out\make\squirrel.windows\x64\kinesis-wallet-setup.exe";
-  npm run deploy -- --from-dry-run --tag="0.1.${env:APPVEYOR_BUILD_NUMBER}";
+  cmd.exe /c "`"${env:SIGNTOOL}`" sign /f ./code-sign.pfx /p %CERTIFICATE_PASSWORD% /fd sha1 /tr http://sha1timestamp.ws.symantec.com/sha1/timestamp /td sha1 /v ${outFile}";
+  cmd.exe /c "`"${env:SIGNTOOL}`" sign /f ./code-sign.pfx /p %CERTIFICATE_PASSWORD% /fd sha256 /tr http://sha256timestamp.ws.symantec.com/sha256/timestamp /td sha256 /as /v ${outFile}";
+  npm run deploy -- --from-dry-run --tag="`'0.1.${env:APPVEYOR_BUILD_NUMBER}`'";
 } else {
   npm test;
 }
