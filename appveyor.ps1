726847a48d5c854a19808024acfc6dde8616a254
Carl Foster <carl.john.foster@gmail.com>
2018-05-16 14:11:35 +1000

    Ensure tests are run

--- appveyor.ps1
+++ appveyor.ps1
@@ -5,4 +5,6 @@ if ($env:APPVEYOR_PULL_REQUEST_NUMBER -eq "") {
     signtool.exe sign /f ./code-sign.pfx /p "%CERTIFICATE_PASSWORD%" /fd sha256 /tr http://sha256timestamp.ws.symantec.com/sha256/timestamp /td sha256 /as /v .\out\make\squirrel.windows\x64\kinesis-wallet-setup.exe
     npm run publish -- --from-dry-run
   }
+} else {
+  npm test
 }
