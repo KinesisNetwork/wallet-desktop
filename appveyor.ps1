604a3ef8e5113e6fa85a9541b0cae9987101932c
Carl Foster <carl.john.foster@gmail.com>
2018-05-16 17:10:40 +1000

    Bad path checker

--- appveyor.ps1
+++ appveyor.ps1
@@ -1,6 +1,6 @@
-Get-ChildItem "C:\Program Files(x86)\Windows Kits\10\bin\";
-Get-ChildItem "C:\Program Files(x86)\Windows Kits\10\bin\x86";
-Get-ChildItem "C:\Program Files(x86)\Windows Kits\10\bin\x64";
+Get-ChildItem "C:\Program Files (x86)\Windows Kits\10\bin\";
+Get-ChildItem "C:\Program Files (x86)\Windows Kits\10\bin\x86";
+Get-ChildItem "C:\Program Files (x86)\Windows Kits\10\bin\x64";
 if ("${env:APPVEYOR_PULL_REQUEST_NUMBER}" -eq "") {
   npm run deploy -- --dry-run --tag="0.1.${env:APPVEYOR_BUILD_NUMBER}";
   cmd.exe /c "`"${env:SIGNTOOL}`" sign /f ./code-sign.pfx /p %CERTIFICATE_PASSWORD% /fd sha1 /tr http://sha1timestamp.ws.symantec.com/sha1/timestamp /td sha1 /v .\out\make\squirrel.windows\x64\kinesis-wallet-setup.exe";
