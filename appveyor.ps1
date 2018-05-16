bc532081a1f3ea7ecb39fb48c094065e0e375052
Carl Foster <carl.john.foster@gmail.com>
2018-05-16 16:17:12 +1000

    Appveyor fixes

--- appveyor.ps1
+++ appveyor.ps1
@@ -1,8 +1,8 @@
 $EnvironmentVariables = Get-ChildItem Env:;
 if ($EnvironmentVariables -notcontains "APPVEYOR_PULL_REQUEST_NUMBER") {
   npm run publish -- --dry-run --tag="0.1.$env:APPVEYOR_BUILD_NUMBER";
-  signtool.exe sign /f ./code-sign.pfx /p "%CERTIFICATE_PASSWORD%" /fd sha1 /tr http://sha1timestamp.ws.symantec.com/sha1/timestamp /td sha1 /v .\out\make\squirrel.windows\x64\kinesis-wallet-setup.exe;
-  signtool.exe sign /f ./code-sign.pfx /p "%CERTIFICATE_PASSWORD%" /fd sha256 /tr http://sha256timestamp.ws.symantec.com/sha256/timestamp /td sha256 /as /v .\out\make\squirrel.windows\x64\kinesis-wallet-setup.exe;
+  "C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Bin\signtool.exe" sign /f ./code-sign.pfx /p "%CERTIFICATE_PASSWORD%" /fd sha1 /tr http://sha1timestamp.ws.symantec.com/sha1/timestamp /td sha1 /v .\out\make\squirrel.windows\x64\kinesis-wallet-setup.exe;
+  "C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Bin\signtool.exe" sign /f ./code-sign.pfx /p "%CERTIFICATE_PASSWORD%" /fd sha256 /tr http://sha256timestamp.ws.symantec.com/sha256/timestamp /td sha256 /as /v .\out\make\squirrel.windows\x64\kinesis-wallet-setup.exe;
   npm run publish -- --from-dry-run;
 } else {
   npm test;
