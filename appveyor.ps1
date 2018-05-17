1010f8d6e74ae03eacac68f5009ab303bd7584f2
Carl Foster <carl.john.foster@gmail.com>
2018-05-17 11:28:48 +1000

    Fix up and operator

--- appveyor.ps1
+++ appveyor.ps1
@@ -1,4 +1,4 @@
-if ("${env:APPVEYOR_PULL_REQUEST_NUMBER}" -ne "" && "${env:APPVEYOR_REPO_TAG}" -eq "true") {
+if ("${env:APPVEYOR_PULL_REQUEST_NUMBER}" -ne "" -and "${env:APPVEYOR_REPO_TAG}" -eq "true") {
   npm run deploy -- --dry-run --arch=x64;
   cmd.exe /c "`"${env:SIGNTOOL}`" sign /f ./code-sign.pfx /p %CERTIFICATE_PASSWORD% /fd sha1 /tr http://sha1timestamp.ws.symantec.com/sha1/timestamp /td sha1 /v `"${env:OUTFILE}`"";
   cmd.exe /c "`"${env:SIGNTOOL}`" sign /f ./code-sign.pfx /p %CERTIFICATE_PASSWORD% /fd sha256 /tr http://sha256timestamp.ws.symantec.com/sha256/timestamp /td sha256 /as /v `"${env:OUTFILE}`"";
