a0bc58a388da0e08cfc1b9c0a518da1636e93165
Carl Foster <carl.john.foster@gmail.com>
2018-05-17 11:24:43 +1000

    Ensure builds on tags of master

--- appveyor.ps1
+++ appveyor.ps1
@@ -1,8 +1,8 @@
-if ("${env:APPVEYOR_PULL_REQUEST_NUMBER}" -eq "") {
+if ("${env:APPVEYOR_PULL_REQUEST_NUMBER}" -ne "" && "${env:APPVEYOR_REPO_TAG}" -eq "true") {
   npm run deploy -- --dry-run --arch=x64;
   cmd.exe /c "`"${env:SIGNTOOL}`" sign /f ./code-sign.pfx /p %CERTIFICATE_PASSWORD% /fd sha1 /tr http://sha1timestamp.ws.symantec.com/sha1/timestamp /td sha1 /v `"${env:OUTFILE}`"";
   cmd.exe /c "`"${env:SIGNTOOL}`" sign /f ./code-sign.pfx /p %CERTIFICATE_PASSWORD% /fd sha256 /tr http://sha256timestamp.ws.symantec.com/sha256/timestamp /td sha256 /as /v `"${env:OUTFILE}`"";
-  npm run deploy -- --from-dry-run --tag "0.1.${env:APPVEYOR_BUILD_NUMBER}";
+  npm run deploy -- --from-dry-run;
 } else {
   npm test;
 }
