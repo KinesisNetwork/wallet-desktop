3f291db5f501cea6cfe9e4222e67f5a4d4b72b2f
Sam Jeston <sam.jeston@gmail.com>
2018-06-08 10:51:22 +1000

    Notifiers on transfer success and failure

--- src/helpers/alert.ts
+++ src/helpers/alert.ts
@@ -4,3 +4,11 @@ export async function formAlert(message: string, key: string) {
   await sweetAlert('Oops!', message, 'error')
   focus(key)
 }
+
+export async function generalFailureAlert(message: string) {
+  return sweetAlert('Oops!', message, 'error')
+}
+
+export async function generalSuccessAlert(message: string) {
+  return sweetAlert('Success', message, 'success')
+}
