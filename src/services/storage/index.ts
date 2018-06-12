ea6726bba993c0e6c9734790c149ed28fdca0f26
Carl Foster <carl.john.foster@gmail.com>
2018-06-12 12:30:21 +1000

    Enable fully persistent state management

--- src/services/storage/index.ts
+++ src/services/storage/index.ts
@@ -1,22 +1,7 @@
-let getElectronStorage
-let setElectronStorage
+import { createWebStorage } from 'redux-persist'
 
-// If we are in webmode, we can't require electron, as it has reference to the fs module
-if (!process.env.IS_WEB) {
-  getElectronStorage = require('./electron').getElectronStorage
-  setElectronStorage = require('./electron').setElectronStorage
-}
-
-import { getWebStorage, setWebStorage } from './webstorage'
-
-export function setStorage<T>(key: string, items: T[], callback: (err: any) => void) {
+export function createStorage(): Storage {
   return process.env.IS_WEB
-    ? setWebStorage(key, items, callback)
-    : setElectronStorage(key, items, callback)
-}
-
-export function getStorage(key: string, callback: (err: any, data: {}) => void) {
-  return process.env.IS_WEB
-    ? getWebStorage(key, callback)
-    : getElectronStorage(key, callback)
+    ? createWebStorage('local')
+    : require('redux-persist-electron-storage').default()
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,25 @@
+import { createStorage } from '@services/storage'
 import { applyMiddleware, compose, createStore, Dispatch } from 'redux'
 import { createEpicMiddleware, Epic } from 'redux-observable'
+import { persistReducer, persistStore, REHYDRATE } from 'redux-persist'
 import { RootAction } from './root-action'
 import { rootEpic, rootReducer, RootState } from './root-reducer'
 
-const epicMiddleware = createEpicMiddleware(rootEpic)
-
 export type Dispatch = Dispatch<RootAction>
-export type Epic = Epic<RootAction, RootState>
+const RehydrateAction = { type: REHYDRATE }
+export type Epic = Epic<RootAction | typeof RehydrateAction, RootState>
 export { RootAction, RootState }
 
-const w = window as any
-const composeEnhancers = w.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(epicMiddleware)))
+export function configureStore() {
+  const epicMiddleware = createEpicMiddleware(rootEpic)
+
+  const persistedReducer = persistReducer({ key: 'root', storage: createStorage() }, rootReducer)
+
+  const w = window as any
+  const composeEnhancers = w.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+  const store = createStore(persistedReducer, composeEnhancers(applyMiddleware(epicMiddleware)))
+  const persistor = persistStore(store)
+
+  return { store, persistor }
+}
