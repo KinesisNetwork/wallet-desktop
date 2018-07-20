4d366f70c53c8573fc412f55a4707a744a00dfe0
Carl Foster <carl.john.foster@gmail.com>
2018-07-20 13:47:14 +1000

    Ensure that git tags are good on all platforms

--- appveyor.ps1
+++ appveyor.ps1
@@ -1,5 +1,6 @@
 if ("${env:APPVEYOR_REPO_TAG}" -eq "True") {
   npm run predeploy:appveyor;
+  npm version "${env:APPVEYOR_REPO_TAG_NAME}" --no-git-tag-version;
   npm run deploy:electron -- --dry-run --arch=x64;
   cmd.exe /c "`"${env:SIGNTOOL}`" sign /f ./code-sign.pfx /p %CERTIFICATE_PASSWORD% /fd sha1 /tr http://sha1timestamp.ws.symantec.com/sha1/timestamp /td sha1 /v `"${env:OUTFILE}`"";
   cmd.exe /c "`"${env:SIGNTOOL}`" sign /f ./code-sign.pfx /p %CERTIFICATE_PASSWORD% /fd sha256 /tr http://sha256timestamp.ws.symantec.com/sha256/timestamp /td sha256 /as /v `"${env:OUTFILE}`"";
