4f93ee59402f2e05dbcfb8838f108529e415c921
Carl Foster <carl.john.foster@gmail.com>
2018-07-20 13:37:01 +1000

    Try and fix the git tagging more

--- appveyor.ps1
+++ appveyor.ps1
@@ -1,5 +1,5 @@
 if ("${env:APPVEYOR_REPO_TAG}" -eq "True") {
-  GIT_TAG="${env:APPVEYOR_REPO_TAG_NAME}" npm run predeploy
+  npm run predeploy:appveyor;
   npm run deploy:electron -- --dry-run --arch=x64;
   cmd.exe /c "`"${env:SIGNTOOL}`" sign /f ./code-sign.pfx /p %CERTIFICATE_PASSWORD% /fd sha1 /tr http://sha1timestamp.ws.symantec.com/sha1/timestamp /td sha1 /v `"${env:OUTFILE}`"";
   cmd.exe /c "`"${env:SIGNTOOL}`" sign /f ./code-sign.pfx /p %CERTIFICATE_PASSWORD% /fd sha256 /tr http://sha256timestamp.ws.symantec.com/sha256/timestamp /td sha256 /as /v `"${env:OUTFILE}`"";
