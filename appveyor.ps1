1ce3ad3e61a4dd80fe3e7ffea09669df869590bd
Carl Foster <carl.john.foster@gmail.com>
2018-07-20 11:03:26 +1000

    Fix up electron deploys

--- appveyor.ps1
+++ appveyor.ps1
@@ -1,6 +1,6 @@
 if ("${env:APPVEYOR_REPO_TAG}" -eq "True") {
-  npm run deploy -- --dry-run --arch=x64;
+  npm run deploy:electron -- --dry-run --arch=x64;
   cmd.exe /c "`"${env:SIGNTOOL}`" sign /f ./code-sign.pfx /p %CERTIFICATE_PASSWORD% /fd sha1 /tr http://sha1timestamp.ws.symantec.com/sha1/timestamp /td sha1 /v `"${env:OUTFILE}`"";
   cmd.exe /c "`"${env:SIGNTOOL}`" sign /f ./code-sign.pfx /p %CERTIFICATE_PASSWORD% /fd sha256 /tr http://sha256timestamp.ws.symantec.com/sha256/timestamp /td sha256 /as /v `"${env:OUTFILE}`"";
-  npm run deploy -- --from-dry-run;
+  npm run deploy:electron -- --from-dry-run;
 }
