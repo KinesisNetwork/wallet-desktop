b44ee389c8bf99c806d8e93d8fa86720b381714f
Arpad <arpad.toth@abx.com>
2018-08-16 11:32:39 +1000

    Add component tests for WalletInfo

---
+++ src/components/__tests__/WalletInfo.test.tsx
@@ -0,0 +1,48 @@
+import * as Enzyme from 'enzyme'
+import { shallow } from 'enzyme';
+import * as Adapter from 'enzyme-adapter-react-16'
+import * as React from 'react'
+
+import { LabelledField } from '@components/LabelledField';
+import { WalletInfo } from '@components/WalletInfo'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe('WalletInfo', () => {
+  it('renders correctly', () => {
+    const wrapper = shallow(<WalletInfo />)
+
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should have a primary heading', () => {
+    const wrapper = shallow(<WalletInfo />)
+
+    const heading = wrapper.find('h1')
+
+    expect(heading.text()).toEqual('Account Information')
+  })
+
+  it('renders four list items in the tab', () => {
+    const wrapper = shallow(<WalletInfo />)
+
+    const tabs = wrapper.find('li')
+
+    expect(tabs).toHaveLength(4)
+    expect(tabs.at(0).text()).toEqual('KAU')
+    expect(tabs.at(1).text()).toEqual('KAG')
+    expect(tabs.at(2).text()).toEqual('KWG')
+    expect(tabs.at(3).text()).toEqual('KWS')
+    expect(tabs.at(1).find('a').hasClass('is-disabled')).toBe(true)
+    expect(tabs.at(2).find('a').hasClass('is-disabled')).toBe(true)
+    expect(tabs.at(3).find('a').hasClass('is-disabled')).toBe(true)
+  })
+
+  it('renders three LabelledField components', () => {
+    const wrapper = shallow(<WalletInfo />)
+
+    const labelledField = wrapper.find(LabelledField)
+
+    expect(labelledField).toHaveLength(3)
+  })
+})
