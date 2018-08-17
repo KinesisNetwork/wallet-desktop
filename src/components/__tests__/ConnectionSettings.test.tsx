92a93e15658c95f7ebbc0a7b010b287c3cc03eac
Arpad <arpad.toth@abx.com>
2018-08-17 15:31:43 +1000

    Create tests to ConnectionSettings

---
+++ src/components/__tests__/ConnectionSettings.test.tsx
@@ -0,0 +1,22 @@
+import { shallow } from 'enzyme';
+import * as React from 'react'
+
+import { ConnectionSettings } from '@components/ConnectionSettings'
+import '../../setupTests'
+
+describe('ConnectionSettings', () => {
+  it('renders correctly', () => {
+    const wrapper = shallow(<ConnectionSettings />)
+
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should render a primary heading, a ConnectionSelector and a ConnectionForm', () => {
+    const wrapper = shallow(<ConnectionSettings />)
+
+    expect(wrapper.find('h1')).toHaveLength(1)
+    expect(wrapper.find('h1').text()).toEqual('Manage Connections')
+    expect(wrapper.find('Connect(ConnectionSelector)')).toHaveLength(1)
+    expect(wrapper.find('Connect(ConnectionForm)')).toHaveLength(1)
+  })
+})
