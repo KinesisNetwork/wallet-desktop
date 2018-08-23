cba6049abfd2fe3bb8aaabd2ef55b3ccb4e4c574
Arpad <arpad.toth@abx.com>
2018-08-24 09:09:15 +1000

    Add tests to WalletList component

--- src/components/__tests__/WalletList.test.tsx
+++ src/components/__tests__/WalletList.test.tsx
@@ -31,4 +31,23 @@ describe('WalletList', () => {
 
     expect(wrapper).toMatchSnapshot()
   })
+
+  it('renders two WalletListItem components and an Add account button', () => {
+    const wrapper = shallow(<WalletList {...props} />)
+
+    expect(wrapper.find('.panel-heading').text()).toEqual('Accounts')
+    expect(wrapper.find('WalletListItem')).toHaveLength(2)
+    expect(wrapper.find('button')).toHaveLength(1)
+    expect(wrapper.find('button').text()).toEqual('Add Account')
+  })
+
+  it('calls the addWallet on clicking the button', () => {
+    const addWalletMock = jest.fn()
+    const wrapper = shallow(<WalletList {...props} addWallet={addWalletMock} />)
+
+    const button = wrapper.find('button')
+    button.simulate('click')
+
+    expect(addWalletMock).toHaveBeenCalled()
+  })
 })
