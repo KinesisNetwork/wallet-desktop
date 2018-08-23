e5456b5f39a61ae4f5eba37065c4633718868058
Arpad <arpad.toth@abx.com>
2018-08-24 09:30:55 +1000

    Add tests to WalletListItem component

--- src/components/__tests__/WalletList.test.tsx
+++ src/components/__tests__/WalletList.test.tsx
@@ -1,7 +1,7 @@
 import { shallow } from 'enzyme';
 import * as React from 'react'
 
-import { WalletList } from '@components/WalletList'
+import { WalletList, WalletListItem } from '@components/WalletList'
 import '../../setupTests'
 
 describe('WalletList', () => {
@@ -49,5 +49,70 @@ describe('WalletList', () => {
     button.simulate('click')
 
     expect(addWalletMock).toHaveBeenCalled()
+  })
+})
+
+describe('WalletListItem', () => {
+  let props
+
+  beforeEach(() => {
+    props = {
+      deleteWallet: () => null,
+      isActive: true,
+      selectWallet: () => null,
+      wallet: {
+        accountName: 'Fred',
+        encryptedPrivateKey: 'qwer',
+        publicKey: '123'
+      }
+    }
+  })
+
+  it('renders correctly', () => {
+    const wrapper = shallow(<WalletListItem {...props} />)
+
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('displays the element as an anchor tag and calls selectWallet on click', () => {
+    const selectWalletMock = jest.fn()
+    const selectWalletMockParam = {
+      accountName: 'Fred',
+      encryptedPrivateKey: 'qwer',
+      publicKey: '123'
+    }
+    const wrapper = shallow(<WalletListItem {...props} selectWallet={selectWalletMock} />)
+
+    const element = wrapper.find('a')
+    expect(element).toHaveLength(1)
+    expect(element.hasClass('is-active')).toBe(true)
+
+    element.simulate('click')
+
+    expect(selectWalletMock).toHaveBeenCalledWith(selectWalletMockParam)
+  })
+
+  it('renders the account name and a delete button', () => {
+    const wrapper = shallow(<WalletListItem {...props} />)
+
+    expect(wrapper.find('.info').text()).toEqual('Fred')
+    expect(wrapper.find('button')).toHaveLength(1)
+  })
+
+  it('calls the deleteWallet method on clicking the button', () => {
+    const deleteWalletMock = jest.fn()
+    const deleteWalletMockParam = {
+      accountName: 'Fred',
+      encryptedPrivateKey: 'qwer',
+      publicKey: '123'
+    }
+    const wrapper = shallow(<WalletListItem {...props} deleteWallet={deleteWalletMock} />)
+
+    const deleteButton = wrapper.find('button')
+    expect(deleteButton.find('i').hasClass('fa-trash-alt')).toBe(true)
+
+    deleteButton.simulate('click')
+
+    expect(deleteWalletMock).toHaveBeenCalledWith(deleteWalletMockParam)
   })
 })
