57b3b6897792897e4767e18e96f204fc44e764fa
Sam Jeston <sam.jeston@gmail.com>
2018-09-13 16:01:07 +1000

    Editable account design

--- src/components/AccountManagement.tsx
+++ src/components/AccountManagement.tsx
@@ -1,31 +1,65 @@
+import { EditableText } from '@components/EditableText'
 import { Sign } from '@containers/Sign'
+import * as copy from 'copy-to-clipboard'
 import * as React from 'react'
 
-const AccountManagement: React.SFC<any> = () =>
-  <div className='section'>
-    <div className='container'>
-      <div className='card' style={{borderRadius: '5px'}}>
-        <div className='card-content'>
-          <div className='has-text-centered'>
-            <p className='title'>Account 1</p>
-            <div style={{position: 'relative'}}>
-              <a className='button is-primary is-outlined'>
-                GC5AIC4OGSVKYOERINIIDXVYMC5KZPNLA65BPZVOFEBFG4CBAIDHG353
-                &nbsp;
-                <i className='far fa-copy' />
-              </a>
-              <button className='button' style={{position: 'absolute', right: 0}}>
-                <span>Advanced</span>
-                <span className='icon is-small'>
-                  <i className='fas fa-angle-down' aria-hidden='true' />
-                </span>
-              </button>
+class AccountManagement extends React.Component<{}, { isToggled: boolean, copied: boolean, isEditing: boolean }> {
+  public state = { isToggled: false, copied: false, isEditing: false }
+  public toggleAdvanced = () => this.setState({isToggled: !this.state.isToggled})
+
+  public render() {
+    return (
+      <div className='section'>
+        <div className='container'>
+          <div className='card' style={{ borderRadius: '5px' }}>
+            <div className='card-content'>
+              <div className='has-text-centered'>
+                <div className='level'>
+                  <div className='level-item'>
+                    <EditableText
+                      isEditing={this.state.isEditing}
+                      value='Account 1'
+                      onChangeHandler={ev => true}
+                      onStartEditing={() => this.setState({isEditing: !this.state.isEditing})}
+                      onStopEditing={() => this.setState({isEditing: !this.state.isEditing})}
+                      opts={{isLarge: true}}
+                      />
+                  </div>
+                </div>
+                <div style={{ position: 'relative' }}>
+                  <div className='tooltip is-tooltip-top' data-tooltip={this.state.copied ? 'Copied!' : 'Copy Public Address' }>
+                    <button className='button is-primary is-outlined' onClick={this.copyPublicKey}>
+                      GC5AIC4OGSVKYOERINIIDXVYMC5KZPNLA65BPZVOFEBFG4CBAIDHG353
+                      &nbsp;
+                      <i className='far fa-copy' />
+                    </button>
+                  </div>
+                  <button className='button inline-button' style={{ position: 'absolute', right: 0, bottom: 0 }} onClick={this.toggleAdvanced}>
+                    <span>Advanced</span>
+                    <span className={`icon is-small ${this.state.isToggled === false && 'is-hidden'}`}>
+                      <i className='fa fa-angle-up'/>
+                    </span>
+                    <span className={`icon is-small ${this.state.isToggled && 'is-hidden'}`}>
+                      <i className='fa fa-angle-down' />
+                    </span>
+                  </button>
+                </div>
+              </div>
+              <div className='expandable' style={{height: this.state.isToggled ? '400px' : '0'}}>
+                <Sign />
+              </div>
             </div>
           </div>
-          <Sign />
         </div>
       </div>
-    </div>
-  </div>
+    )
+  }
+
+  private copyPublicKey = () => {
+    this.setState({copied: true})
+    copy('GC5AIC4OGSVKYOERINIIDXVYMC5KZPNLA65BPZVOFEBFG4CBAIDHG353')
+    setTimeout(() => this.setState({copied: false}), 5000)
+  }
+}
 
 export { AccountManagement }
--- src/components/EditableText.tsx
+++ src/components/EditableText.tsx
@@ -6,20 +6,23 @@ interface EditableProps {
   onChangeHandler: React.ChangeEventHandler<HTMLInputElement>
   onStopEditing: React.FocusEventHandler<HTMLInputElement>
   onStartEditing: React.UIEventHandler<HTMLInputElement>
+  opts?: { isLarge: boolean }
 }
 export class EditableText extends React.Component<EditableProps> {
   render() {
+    const isLarge = this.props.opts && this.props.opts.isLarge
+
     return (
       <div className={this.controlClasses()}>
         <input
-          className={this.inputClasses()}
+          className={this.inputClasses() + (isLarge ? ' is-large' : '')}
           value={this.props.value}
           onChange={this.props.onChangeHandler}
           onBlur={this.props.onStopEditing}
           onClick={this.onClick}
         />
         {!this.props.isEditing && (
-          <span className="icon is-right">
+          <span className="icon is-right" style={isLarge ? {fontSize: '24px'} : {}}>
             <i className="fa fa-edit" />
           </span>
         )}
