d8a357582d6f2cee293b02e2e88f30c935b18255
Arpad <arpad.toth@abx.com>
2018-09-13 14:02:26 +1000

    Setup close modal logic

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,34 +1,70 @@
+import { closeModal } from '@actions'
+import { RootState } from '@store';
 import * as React from 'react'
+import { connect } from 'react-redux'
+
+const mapStateToProps = (state: RootState) => ({
+  isModalActive: state.modals.modalDisplay
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  closeModal: () => (
+    dispatch(closeModal())
+  )
+})
+
+type Props = ReturnType<typeof mapStateToProps> & ReturnType<typeof mapDispatchToProps>
 
 let onboarding
 if (process.env.IS_WEB) {
   onboarding = require('../onboarding_stickers_Artboard.svg')
 }
 
-export const Modal: React.SFC = () => (
-  <main className="modal is-active">
-    <div className="modal-background" />
-    <section className="modal-content has-background-grey-dark section" style={{ width: '60rem' }}>
-      <h1 className="is-size-3 has-text-weight-semibold has-text-grey-lighter has-text-centered" style={{ letterSpacing: '1px', marginBottom: '3rem' }}>
-        Your new wallet is now active
-      </h1>
-      <div className="columns is-vcentered" style={{ marginBottom: '3rem' }}>
-        <div className="column">
-          <div className="content has-text-grey-lighter has-text-weight-semibold is-size-5">
-            <p style={{ letterSpacing: '3px' }}>Buy Kinesis from the exchange and store it in your wallet.</p>
-            <p style={{ letterSpacing: '3px' }}>Kinesis is the world's most stable currency, backed 1:1 against gold (KAU) and silver (KAG) bullion.</p>
-            <a href="#" className="is-link is-size-6">Learn more</a>
+export class ModalPresentation extends React.Component<Props> {
+  closeModal = () => {
+    this.props.closeModal()
+  }
+
+  render() {
+    return (
+      <main className={`modal ${this.props.isModalActive ? 'is-active' : ''}`}>
+        <div className="modal-background" />
+        <section className="modal-content has-background-grey-dark section" style={{ width: '60rem' }}>
+          <h1
+            className="is-size-3 has-text-weight-semibold has-text-grey-lighter has-text-centered"
+            style={{ letterSpacing: '1px', marginBottom: '3rem' }}
+          >
+            Your new wallet is now active
+          </h1>
+          <div className="columns is-vcentered" style={{ marginBottom: '3rem' }}>
+            <div className="column">
+              <div className="content has-text-grey-lighter has-text-weight-semibold is-size-5">
+                <p style={{ letterSpacing: '3px' }}>Buy Kinesis from the exchange and store it in your wallet.</p>
+                <p style={{ letterSpacing: '3px' }}>Kinesis is the world's most stable currency, backed 1:1 against gold (KAU) and silver (KAG) bullion.</p>
+                <a className="is-link is-size-6">Learn more</a>
+              </div>
+            </div>
+            <div className="column">
+              <figure className="image is-4by3">
+                <img src={onboarding ? onboarding : './onboarding_stickers_Artboard.svg'} className="" />
+              </figure>
+            </div>
           </div>
-        </div>
-        <div className="column">
-          <figure className="image is-4by3">
-            <img src={onboarding ? onboarding : './onboarding_stickers_Artboard.svg'} className="" />
-          </figure>
-        </div>
-      </div>
-      <div className="is-pulled-right">
-        <button className="button has-text-grey-darker is-uppercase is-primary">OK</button>
-      </div>
-    </section>
-  </main>
-)
+          <div className="is-pulled-right">
+            <button
+              className="button has-text-grey-darker is-uppercase is-primary"
+              onClick={() => this.closeModal()}
+            >OK</button>
+          </div>
+        </section>
+      </main>
+    )
+  }
+}
+
+const ConnectedModalPresentation = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ModalPresentation)
+
+export { ConnectedModalPresentation as Modal }
---
+++ src/store/reducers/modal.ts
@@ -0,0 +1,19 @@
+import { closeModal } from '@actions'
+import { RootAction } from '@store'
+import { combineReducers } from 'redux';
+import { getType } from 'typesafe-actions';
+
+export interface ModalState {
+  readonly modalDisplay: boolean
+}
+
+export const modals = combineReducers<ModalState, RootAction>({
+  modalDisplay: (state = true, action) => {
+    switch (action.type) {
+      case getType(closeModal):
+        return false
+      default:
+        return state
+    }
+  },
+})
