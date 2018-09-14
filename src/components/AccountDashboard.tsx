90f875d2483238ee48ffb7c2808959aa0adee9dd
Carl Foster <carl.foster@abx.com>
2018-09-14 10:24:02 +1000

    Set up a switcher for the currencies

--- src/components/AccountDashboard.tsx
+++ src/components/AccountDashboard.tsx
@@ -2,12 +2,22 @@ import * as React from 'react'
 
 import { CurrencySelector } from '@containers/CurrencySelector'
 
-import * as logo from '@icons/kau-icon.svg'
-import { RootRoutes } from '@types'
+import * as kagLogo from '@icons/kag-icon.svg'
+import * as kauLogo from '@icons/kau-icon.svg'
+import { RootState } from '@store'
+import { Currency, RootRoutes } from '@types'
 import * as sleeping from 'images/wally-sleeping.svg'
+import { connect } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 
-export const AccountDashboard: React.SFC = () => (
+const mapStateToProps = (state: RootState) => ({
+  currency: state.connections.currentCurrency,
+  balance: state.accounts.accountInfo.balance,
+})
+
+type Props = ReturnType<typeof mapStateToProps>
+
+const AccountDashboardPresentation: React.SFC<Props> = props => (
   <React.Fragment>
     <div className="container">
       <CurrencySelector />
@@ -15,13 +25,20 @@ export const AccountDashboard: React.SFC = () => (
         <div className="level">
           <div className="level-item">
             <figure className="image is-128x128">
-              <img src={logo} className="is-rounded" />
+              <img
+                src={props.currency === Currency.KAU ? kauLogo : kagLogo}
+                className="is-rounded"
+              />
             </figure>
           </div>
         </div>
         <div className="level">
           <div className="level-item">
-            <h1 className="title is-size-2 has-text-primary has-text-weight-bold">40.000 KAU</h1>
+            <h1
+              className={`title is-size-2 has-text-weight-bold ${
+                props.currency === Currency.KAU ? 'has-text-primary' : 'has-text-grey-light'
+              }`}
+            >{`${props.balance} ${props.currency}`}</h1>
           </div>
         </div>
         <div className="level">
@@ -46,7 +63,7 @@ export const AccountDashboard: React.SFC = () => (
             <div>
               <h2 className="subtitle">There's nothing here...</h2>
               <h2 className="subtitle">
-                Once KAU transactions have been made, we'll show the history here!
+                Once {props.currency} transactions have been made, we'll show the history here!
               </h2>
               <h2 className="subtitle">Need to buy Kinesis?</h2>
               <div>
@@ -60,4 +77,6 @@ export const AccountDashboard: React.SFC = () => (
   </React.Fragment>
 )
 
-AccountDashboard.displayName = 'AccountDashboard'
+const AccountDashboard = connect(mapStateToProps)(AccountDashboardPresentation)
+
+export { AccountDashboard }
