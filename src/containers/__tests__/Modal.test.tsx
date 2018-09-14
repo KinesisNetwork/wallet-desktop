91a6f6716d852023807cc904e80b28dba08853a4
Arpad <arpad.toth@abx.com>
2018-09-14 13:03:22 +1000

    Add test to onClick

--- src/containers/__tests__/Modal.test.tsx
+++ src/containers/__tests__/Modal.test.tsx
@@ -16,5 +16,24 @@ describe('Modal', () => {
     const wrapper = shallow(<ModalPresentation {...props} />)
 
     expect(wrapper).toMatchSnapshot()
+    expect(wrapper.find('.modal').hasClass('is-active')).toBe(true)
+  })
+
+  it('should not render modal if the onboarding has already been done', () => {
+    const wrapper = shallow(<ModalPresentation {...props} hasOnBoarded={true} />)
+
+    expect(wrapper.find('.modal').hasClass('is-active')).toBe(false)
+  })
+
+  it('calls closeModal and completeOnBoarding methods on clicking the button', () => {
+    const closeModalStub = jest.fn()
+    const completeOnBoardingStub = jest.fn()
+    const wrapper = shallow(<ModalPresentation {...props} closeModal={closeModalStub} completeOnBoarding={completeOnBoardingStub} />)
+
+    const button = wrapper.find('button')
+    button.simulate('click')
+
+    expect(closeModalStub).toHaveBeenCalled()
+    expect(completeOnBoardingStub).toHaveBeenCalled()
   })
 })
