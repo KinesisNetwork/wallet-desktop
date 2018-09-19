9d25baa7cbbd51108773d40a3e5f9755ccee9026
Carl Foster <carl.foster@abx.com>
2018-09-19 15:56:48 +1000

    Fix up tests

--- src/store/epics/__tests__/accounts.test.ts
+++ src/store/epics/__tests__/accounts.test.ts
@@ -4,6 +4,7 @@ import { toArray } from 'rxjs/operators'
 
 import { accountLoadRequest } from '@actions'
 import { loadAccount$ } from '../accounts'
+import { epicTest } from './helpers'
 
 describe('Accounts epic', () => {
   describe('loadAccount$', () => {
@@ -11,39 +12,41 @@ describe('Accounts epic', () => {
     const connection = 'connection'
     const error = 'error'
     const publicKey = 'public key'
-    const connectionState = {
-      connections: {
-        currentConnection: connection,
-      },
-    }
+    const connectionState = {}
     const store$: any = of(connectionState)
 
     const action$ = ActionsObservable.from([accountLoadRequest(publicKey)])
 
-    it('success', done => {
-      const getCurrentConnection = jest.fn(() => connection)
+    it('success', async () => {
       const loadAccount = jest.fn(() => Promise.resolve(account))
+      const getCurrentConnection = jest.fn()
+      const getTransactions = jest.fn(() => Promise.resolve([]))
 
-      loadAccount$(action$, store$, { getCurrentConnection, loadAccount } as any)
-        .pipe(toArray())
-        .subscribe(actions => {
-          expect(actions).toEqual([])
-          done()
-        })
+      await epicTest({
+        epic: loadAccount$,
+        inputActions: [accountLoadRequest(publicKey)],
+        state: {
+          router: { location: { pathname: '/dashboard' } },
+          connections: {},
+        },
+        dependencies: { loadAccount, getCurrentConnection, getTransactions },
+      })
     })
 
-    it('failure', done => {
-      const expectedOutputActions = []
-
+    it('failure', async () => {
       const getCurrentConnection = jest.fn(() => connection)
       const loadAccount = jest.fn(() => Promise.reject(error))
+      const getTransactions = jest.fn(() => Promise.resolve([]))
 
-      loadAccount$(action$, store$, { getCurrentConnection, loadAccount } as any)
-        .pipe(toArray())
-        .subscribe(actions => {
-          expect(actions).toEqual(expectedOutputActions)
-          done()
-        })
+      await epicTest({
+        epic: loadAccount$,
+        inputActions: [accountLoadRequest(publicKey)],
+        state: {
+          router: { location: { pathname: '/dashboard' } },
+          connections: {},
+        },
+        dependencies: { loadAccount, getCurrentConnection, getTransactions },
+      })
     })
   })
 })
