8b0bf84703163e3da68c6135e8b7ce2b8574a088
Alex Revell <alex.revell@abx.com>
2018-09-27 15:30:29 +1000

    ABX-4538 Update WalletCreation and Recovery screens to use Modal component

--- src/containers/WalletCreation/PasswordConfirmModal.tsx
+++ src/containers/WalletCreation/PasswordConfirmModal.tsx
@@ -1,50 +1,50 @@
-import { RootRoutes, WalletCreationRoutes } from '@types'
 import * as React from 'react'
 import { Link } from 'react-router-dom'
+
+import { Modal } from '@components/Modal'
+import { RootRoutes, WalletCreationRoutes } from '@types'
 
 interface ModalProps {
   isActive: boolean
   closeModal: () => any
 }
-export const PasswordConfirmModal: React.SFC<ModalProps> = props => (
-  <div className={`modal ${props.isActive ? 'is-active' : ''}`}>
-    <div className="modal-background" />
-    <div className="modal-content">
-      <article className="message">
-        <div className="message-header">
-          <span className="icon is-medium has-text-danger ">
-            <i className="fal fa-2x fa-exclamation-circle" />
-          </span>
-          <span className="title is-marginless">Keep your password safe</span>
-          <button className="delete" aria-label="close" onClick={props.closeModal} />
+export const PasswordConfirmModal: React.SFC<ModalProps> = ({ closeModal, isActive }) => (
+  <Modal hasBackground={true} isActive={isActive}>
+    <article className="message">
+      <div className="message-header">
+        <span className="icon is-medium has-text-warning ">
+          <i className="fal fa-2x fa-exclamation-circle" />
+        </span>
+        <span className="title is-marginless">Keep your password safe</span>
+        <button className="delete" aria-label="close" onClick={closeModal} />
+      </div>
+      <div className="message-body">
+        <div className="content">
+          <p>
+            For security reasons we do not store your password anywhere. This means that if you
+            forget your password we cannot reset it for you.
+          </p>
+          <p>
+            <a>Learn more</a>
+          </p>
         </div>
-        <div className="message-body">
-          <div className="content">
-            <p>
-              For security reasons we do not store your password anywhere. This means that if you forget your password we cannot reset it for you.
-            </p>
-            <p>
-              <a>Learn more</a>
-            </p>
+        <div className="field is-grouped is-grouped-right">
+          <div className="control">
+            <button className="button is-text" onClick={closeModal}>
+              Back
+            </button>
           </div>
-          <div className="field is-grouped is-grouped-right">
-            <div className="control">
-              <button className="button is-text" onClick={props.closeModal}>
-                Back
-              </button>
-            </div>
-            <div className="control">
-              <Link
-                to={RootRoutes.create + WalletCreationRoutes.second}
-                className="button is-primary"
-                onClick={props.closeModal}
-              >
-                Continue
-              </Link>
-            </div>
+          <div className="control">
+            <Link
+              to={RootRoutes.create + WalletCreationRoutes.second}
+              className="button is-primary"
+              onClick={closeModal}
+            >
+              Continue
+            </Link>
           </div>
         </div>
-      </article>
-    </div>
-  </div>
+      </div>
+    </article>
+  </Modal>
 )
