c3f7cf5946e7a4085501593a357a712f1e8cbbc8
Alex Revell <alex.revell@abx.com>
2018-09-27 15:28:21 +1000

    ABX-4538 Update Onboarding to use Modal

--- src/containers/Onboarding.tsx
+++ src/containers/Onboarding.tsx
@@ -1,18 +1,19 @@
-import { closeModal, completeOnBoarding } from '@actions'
-import { RootState } from '@store';
 import * as React from 'react'
 import { connect } from 'react-redux'
 
+import { closeModal, completeOnBoarding } from '@actions'
+import { Modal } from '@components/Modal'
 import * as onboarding from '@images/onboarding_stickers_Artboard.svg'
+import { RootState } from '@store'
 
-const mapStateToProps = (state: RootState) => ({
-  isModalActive: state.modals.modalDisplay,
-  hasOnBoarded: state.settings.onBoarding
+const mapStateToProps = ({ modals, settings }: RootState) => ({
+  isModalActive: modals.modalDisplay,
+  hasOnBoarded: settings.onBoarding,
 })
 
 const mapDispatchToProps = {
   closeModal,
-  completeOnBoarding
+  completeOnBoarding,
 }
 
 type Props = ReturnType<typeof mapStateToProps> & typeof mapDispatchToProps
@@ -24,10 +25,11 @@ export class OnboardingPresentation extends React.Component<Props> {
   }
 
   render() {
+    const { hasOnBoarded, isModalActive } = this.props
+
     return (
-      <main className={`modal ${this.props.isModalActive && !this.props.hasOnBoarded ? 'is-active' : ''}`}>
-        <div className="modal-background" />
-        <section className="modal-content box" style={{ width: '55rem' }}>
+      <Modal hasBackground={true} isActive={isModalActive && !hasOnBoarded}>
+        <div className="box">
           <h1 className="title is-2 has-text-primary is-uppercase has-text-centered">
             Your new wallet is now active
           </h1>
@@ -35,33 +37,38 @@ export class OnboardingPresentation extends React.Component<Props> {
             <div className="column">
               <div className="content has-text-grey-lighter is-size-4">
                 <p>Buy Kinesis from the exchange and store it in your wallet.</p>
-                <p>Kinesis is the world's most stable currency, backed 1:1 against gold (KAU) and silver (KAG) bullion.</p>
+                <p>
+                  Kinesis is the world's most stable currency, backed 1:1 against gold (KAU) and
+                  silver (KAG) bullion.
+                </p>
                 <a className="is-link is-size-6">Learn more</a>
               </div>
             </div>
             <div className="column">
               <figure className="image is-4by3">
-                <img src={onboarding ? onboarding : './onboarding_stickers_Artboard.svg'} className="" />
+                <img
+                  src={onboarding ? onboarding : './onboarding_stickers_Artboard.svg'}
+                  className=""
+                />
               </figure>
             </div>
           </div>
           <div className="field is-grouped is-grouped-right">
             <div className="control">
-              <button
-                className="button is-primary"
-                onClick={() => this.closeModal()}
-              >OK</button>
+              <button className="button is-primary" onClick={this.closeModal}>
+                OK
+              </button>
             </div>
           </div>
-        </section>
-      </main>
+        </div>
+      </Modal>
     )
   }
 }
 
-const ConnectedOnboardingPresentation = connect(
+const Onboarding = connect(
   mapStateToProps,
-  mapDispatchToProps
+  mapDispatchToProps,
 )(OnboardingPresentation)
 
-export { ConnectedOnboardingPresentation as Onboarding }
+export { Onboarding }
