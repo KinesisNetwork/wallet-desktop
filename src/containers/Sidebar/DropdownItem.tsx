8935dd6ad6aacc15d245b8bdc0e58e587b54f434
Carl Foster <carl.john.foster@gmail.com>
2018-09-28 15:16:42 +1000

    Update dropdown styling and keep the dom cleaner

--- src/containers/Sidebar/DropdownItem.tsx
+++ src/containers/Sidebar/DropdownItem.tsx
@@ -15,27 +15,29 @@ export const DropdownItem: React.SFC<Props> = ({
   isImport = false,
   isActive = false,
   onClick,
-  subtitle,
   title,
   titleClassName,
 }) => (
   <div className={`dropdown-item is-paddingless ${isActive && 'is-active'}`} onClick={onClick}>
     <a className="button is-radiusless is-fullwidth is-shadowless" style={{ overflow: 'visible' }}>
-      <span className={`icon ${!icon && 'is-hidden'}`}>
-        <i className={`fal fa-lg fa-${icon}`} />
-      </span>
-      <span className={`icon ${!isActive && 'is-hidden'}`}>
-        <i className="fal fa-lg fa-check-circle" />
-      </span>
-      <span className={`icon ${(isActive || icon) && 'is-hidden'}`} />
-      <div className="dropdown-item-text vertical-spaced">
-        <p className={titleClassName}>{title}</p>
-        {subtitle && <p className="is-size-7">{subtitle}</p>}
-      </div>
-      <span className={`icon ${!isImport && 'is-hidden'} tooltip`} data-tooltip="Imported account">
-        <i className={`fal fa-sm fa-download`} />
-      </span>
-      <span className={`icon ${isImport && 'is-hidden'}`} />
+      {icon && (
+        <span className={`icon`}>
+          <i className={`fal fa-lg fa-${icon}`} />
+        </span>
+      )}
+      {isActive && (
+        <span className={`icon`}>
+          <i className="fal fa-lg fa-check-circle" />
+        </span>
+      )}
+      {!isActive && !icon && <span className={`icon`} />}
+      <span className={titleClassName}>{title}</span>
+      {isImport && (
+        <span className={`icon tooltip`} data-tooltip="Imported account">
+          <i className={`fal fa-sm fa-download`} />
+        </span>
+      )}
+      {!isImport && <span className={`icon`} />}
     </a>
   </div>
 )
