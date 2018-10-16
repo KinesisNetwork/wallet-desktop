18cafabd3a8014a996dae29d3463434bb087b0a1
Alex Revell <alex.revell@abx.com>
2018-10-16 16:02:06 +1000

    ABX-4613 Disable multiple submit of same payment
    * update Confirmation page to disable button while submitting transaction
    * update transfer epic to use exhaustMap operator to block subsequent requests
    initial one has completed
    * update TransferDetails and Summary types and imports
    * update TransferSummary test

--- src/containers/__tests__/TransferCurrency/TransferSummary.test.tsx
+++ src/containers/__tests__/TransferCurrency/TransferSummary.test.tsx
@@ -1,11 +1,19 @@
-import { TransferSummaryPresentation } from '@containers/TransferCurrency/TransferSummary'
 import { shallow } from 'enzyme'
 import * as React from 'react'
+
+import { TransferSummaryPresentation } from '@containers/TransferCurrency/TransferSummary'
+import { Currency } from '@types'
 import '../../../setupTests'
 
 describe('TransferSummary', () => {
+  const props = {
+    amount: '5',
+    currency: Currency.KAU,
+    fee: '1',
+    memo: 'memo',
+  }
   it('should render correctly', () => {
-    const wrapper = shallow(<TransferSummaryPresentation />)
+    const wrapper = shallow(<TransferSummaryPresentation {...props} />)
 
     expect(wrapper).toMatchSnapshot()
   })
