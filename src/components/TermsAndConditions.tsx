ffd1782fad067d03609dfbb3b8ef82c01100a7e5
Jeff Teng <jeff.teng@abx.com>
2019-03-18 10:49:04 +1000

    📚 Docs: Rephrase the term and service sentence

--- src/components/TermsAndConditions.tsx
+++ src/components/TermsAndConditions.tsx
@@ -17,7 +17,11 @@ const TermsAndConditionsPresentation: React.SFC<Props> = props => (
         onChange={props.onConfirmChange}
       />
       <label htmlFor="tcs">
-        I agree with the <a href="https://kinesis.money/en/terms-of-use/" target="_blank"> Terms of Service</a>
+        I acknowledge that I have read and agreed to the{' '}
+        <a href="https://kinesis.money/documents/Kinesis_Terms_Of_Use.pdf" target="_blank">
+          {' '}
+          Terms of Use
+        </a>
       </label>
     </div>
   </div>
